ed03e92db2ec1c4e81427966491fc0369ac2dbf1
Victor Querino Martins <123746753+VictorQMm@users.noreply.github.com>
2024-05-21 12:33:56 -0300

    att

--- Conceitos/Revisão/exemplos/EntradaComScanner.java
+++ Conceitos/Revisão/exemplos/EntradaComScanner.java
@@ -10,13 +10,13 @@ public class EntradaComScanner {
             comprimento = sc.nextFloat();
             
             System.out.println("Entre com a largura");
-            sc = new Scanner(System.in);
             largura = sc.nextFloat();
             
             area = comprimento * largura;
-            perimetro = comprimento * 2 +largura * 2;
+            perimetro = comprimento * 2 + largura * 2;
             System.out.println("Area do retangulo: " + area);
             System.out.println("Perimetro do retangulo: " + perimetro);
+            
         } catch (NumberFormatException e) {
             System.out.println("Houve erro na conversao, digite apenas caracteres numericos");
         }
